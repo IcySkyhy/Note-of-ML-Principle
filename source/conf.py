a4f0a3789faf1914300be6ab38ce95f10ddf1fd0
IcySkyhy <huyan23@mails.tsinghua.edu.cn>
2026-01-31 10:47:50 +0800

    Update#02@20260131

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,10 @@ author = 'IcySkyhy'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables']
+extensions = [
+    'sphinx_rtd_theme',  # 最好显式加上这个，虽然不是强制的
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
