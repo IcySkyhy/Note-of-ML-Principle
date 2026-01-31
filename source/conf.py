87c1a4105104728a2d38fd69551e2fdb7aaafc63
IcySkyhy <huyan23@mails.tsinghua.edu.cn>
2026-01-31 13:14:11 +0800

    Update#05@20260131: ChangeProjectName

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Note-of-ML-Principle'
+project = 'IcySky\'s ML Notes'
 copyright = '2026, IcySkyhy'
 author = 'IcySkyhy'
 
